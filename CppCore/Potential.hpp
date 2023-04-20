3d818483a5a6a819c6a5822177745005ab120019
Rohit Goswami <rog32@hi.is>
2023-04-20 15:16:45 +0000

    ENH: Initialize skeleton for Potential call

---
+++ CppCore/Potential.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "pot_types.hpp"
+
+namespace rgpot {
+class Potential {
+public:
+  // Constructor takes potential type
+  Potential(PotType inp_type) : m_type(inp_type) {}
+
+  // Operator() to calculate energy and forces for the given coordinates and
+  // atom types
+  virtual std::pair<double, AtomMatrix>
+  operator()(const Eigen::Ref<const AtomMatrix> &positions,
+             const Eigen::Ref<const VectorXi> &atmtypes,
+             const Eigen::Ref<const Eigen::Matrix3d> &box) const = 0;
+
+  virtual ~Potential() = default;
+
+private:
+  PotType m_type;
+};
+} // namespace rgpot
