8911ba45ddd1be93c0fc2d1c171333e7634bec39
Rohit Goswami <rog32@hi.is>
2023-04-20 18:00:50 +0000

    ENH: Add an example

---
+++ CppCore/examples/calling_ljpot.cc
@@ -0,0 +1,19 @@
+#include "../src/LennardJones/LJPot.hpp"
+#include <cstdlib>
+
+int main(void) {
+  using namespace rgpot;
+  auto ljpot = LJPot();
+  AtomMatrix positions{
+      {1, 2, 3},      //
+      {4, 5, 6},      //
+      {1.5, 2.5, 3.5} //
+  };
+  Eigen::VectorXi atmtypes{{0, 0, 0}};
+  Eigen::Matrix3d box{{15, 0, 0}, //
+                      {0, 20, 0}, //
+                      {0, 0, 30}};
+  auto [energy, forces] = ljpot(positions, atmtypes, box);
+  fmt::print("Got energy {}\n Forces:\n{}", energy, fmt::streamed(forces));
+  return EXIT_SUCCESS;
+}
