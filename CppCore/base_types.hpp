581c497862856d1e3c0901a677df403341a8ca6a
Rohit Goswami <rog32@hi.is>
2023-04-20 14:48:57 +0000

    ENH: Port more over from kbzoo

---
+++ CppCore/base_types.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <algorithm>
+#include <any>
+#include <cctype>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <regex>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+
+#include <cxxabi.h>
+
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <fmt/os.h>
+#include <fmt/ostream.h>
+#include <fmt/ranges.h>
+#include <spdlog/fmt/ostr.h>
+#include <spdlog/spdlog.h>
+
+#include <Eigen/Dense>
+#include <Eigen/Eigenvalues>
+
+using namespace Eigen;
+using namespace std::string_literals; // For ""s
+
+namespace rgpot {
+using AtomMatrix =
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+using AtomVector =
+    Eigen::Matrix<double, Eigen::Dynamic, 1>; // Same as a normal VectorXd
+                                              // The potentials
+} // namespace rgpot
---
+++ CppCore/pot_types.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "base_types.hpp"
+
+namespace rgpot {
+
+enum class PotType {
+  UNKOWN = 0,
+  CUH2
+};
+
+}
