d2b3502afc4ad50ab5563bedc440052e8603d440
Rohit Goswami <rog32@hi.is>
2023-11-01 04:16:39 +0000

    MAINT: Satisfy linter

--- CppCore/gtests/CuH2PotTest.cc
+++ CppCore/gtests/CuH2PotTest.cc
@@ -5,36 +5,37 @@
 #include "../src/CuH2/CuH2Pot.hpp"
 
 TEST_CASE("CuH2Pot Energy and Forces", "[CuH2Pot]") {
-    using namespace rgpot;
-    auto cuh2pot = CuH2Pot();
-    AtomMatrix positions{
-        {0.63940268750835, 0.90484742551374, 6.97516498544584}, // Cu
-        {3.19652040936288, 0.90417430354811, 6.97547796369474}, // Cu
-        {8.98363230369760, 9.94703496017833, 7.83556854923689}, // H
-        {7.64080177576300, 9.94703114803832, 7.83556986121272}  // H
-    };
-    Eigen::VectorXi atmtypes{{29, 29, 1, 1}};
-    Eigen::Matrix3d box{
-        {15.345599999999999, 0, 0}, //
-        {0, 21.702000000000002, 0}, //
-        {0, 0, 100.00000000000000}  //
-    };
-    auto [energy, forces] = cuh2pot(positions, atmtypes, box);
+  using namespace rgpot;
+  auto cuh2pot = CuH2Pot();
+  AtomMatrix positions{
+      {0.63940268750835, 0.90484742551374, 6.97516498544584}, // Cu
+      {3.19652040936288, 0.90417430354811, 6.97547796369474}, // Cu
+      {8.98363230369760, 9.94703496017833, 7.83556854923689}, // H
+      {7.64080177576300, 9.94703114803832, 7.83556986121272}  // H
+  };
+  Eigen::VectorXi atmtypes{{29, 29, 1, 1}};
+  Eigen::Matrix3d box{
+      {15.345599999999999, 0, 0}, //
+      {0, 21.702000000000002, 0}, //
+      {0, 0, 100.00000000000000}  //
+  };
+  auto [energy, forces] = cuh2pot(positions, atmtypes, box);
 
-    double expected_energy = -2.7114096242662238;
-    AtomMatrix expected_forces{
-        {1.4919411183978113,  -3.9273058476626193E-004,  1.8260603127768336E-004},
-        {-1.4919411183978113,  3.9273058476626193E-004, -1.8260603127768336E-004},
-        {-4.9118653085630006, -1.3944215503304855E-005,  4.7990036210569753E-006},
-        {4.9118653085630006,   1.3944215503304855E-005, -4.7990036210569753E-006}
-    };
+  double expected_energy = -2.7114096242662238;
+  AtomMatrix expected_forces{
+      {1.4919411183978113, -3.9273058476626193E-004, 1.8260603127768336E-004},
+      {-1.4919411183978113, 3.9273058476626193E-004, -1.8260603127768336E-004},
+      {-4.9118653085630006, -1.3944215503304855E-005, 4.7990036210569753E-006},
+      {4.9118653085630006, 1.3944215503304855E-005, -4.7990036210569753E-006}};
 
-    REQUIRE_THAT(energy, Catch::Matchers::WithinAbs(expected_energy, 1e-6));
+  REQUIRE_THAT(energy, Catch::Matchers::WithinAbs(expected_energy, 1e-6));
 
-    // For forces, you may need to loop over each element if there isn't a direct Catch2 matcher for matrices.
-    for (int i = 0; i < forces.rows(); ++i) {
-        for (int j = 0; j < forces.cols(); ++j) {
-            REQUIRE_THAT(forces(i, j), Catch::Matchers::WithinAbs(expected_forces(i, j), 1e-6));
-        }
+  // For forces, you may need to loop over each element if there isn't a direct
+  // Catch2 matcher for matrices.
+  for (int i = 0; i < forces.rows(); ++i) {
+    for (int j = 0; j < forces.cols(); ++j) {
+      REQUIRE_THAT(forces(i, j),
+                   Catch::Matchers::WithinAbs(expected_forces(i, j), 1e-6));
     }
+  }
 }
